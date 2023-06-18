a2f28baf4760b89d79b28754290c77e8fee33b65
arunkumarsubburaj <arunkumar.subburaj@ideas2it.com>
2023-06-18 20:00:21 +0530

    popover enter key select fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,50 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { IonSelect, IonSelectOption } from '@ionic/react';
 
 import './main.css';
 
 function Example() {
+
+  const [currentFruit, setCurrentFruit] = useState('');
+  const fruitSelectRef = useRef(null);
+
+  useEffect(() => {
+    function handleKeyup(e: any) {
+      if(e.key == "Enter" && e.target.closest("ion-select-popover")){
+        console.log(fruitSelectRef.current);
+        var popoverEle = e.target.closest("ion-popover");
+        console.log(popoverEle.lastFocus.value);
+        setCurrentFruit(popoverEle.lastFocus.value);
+        popoverEle.dismiss();
+      }
+    }
+    document.addEventListener('keyup', handleKeyup);
+    return () => {
+      document.removeEventListener('keyup', handleKeyup);
+    };
+  }, []);
+  // The empty dependency array ensures the event listener is only added once
+
+  interface Fruits {
+    value: string;
+    viewValue: string;
+  }
+  const fruits: Fruits[] = [{
+    value: "apple", viewValue: "Apple"
+  }, {
+    value: "orange", viewValue: "Orange"
+  }, {
+    value: "banana", viewValue: "Banana"
+  }, {
+    value: "kiwi", viewValue: "Kiwi"
+  }];
   return (
-    <IonSelect placeholder="Select fruit">
-      <IonSelectOption value="apples">Apples</IonSelectOption>
-      <IonSelectOption value="oranges">Oranges</IonSelectOption>
-      <IonSelectOption value="bananas">Bananas</IonSelectOption>
+    <IonSelect placeholder="Select fruit" ref={fruitSelectRef} interface='popover' value={currentFruit}>
+      {
+        fruits.map((fruit, i) => (
+          <IonSelectOption key={i} value={fruit.value} >{fruit.viewValue}</IonSelectOption>
+        ))
+      }
     </IonSelect>
   );
 }
